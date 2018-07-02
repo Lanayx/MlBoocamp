9b7906913a4432d66b4b322b240478175356581c
Vladimir Shchur <shchurvladimir@gmail.com>
2018-07-02 12:25:11 +0300

    Extract dataset complete

--- Program.fs
+++ Program.fs
@@ -6,74 +6,22 @@ open System.Collections.Generic
 open Newtonsoft.Json
 open Microsoft.ML
 open Microsoft.ML.Data
-open Microsoft.ML.Data
-
-type UserData =
-    {
-        // Category: byte
-        Metrics: Dictionary<int, (byte*byte)>
-        // Days: byte
-        Vote: byte
-    }
-
-let deserializeMetric metric =
-    let obj = JsonConvert.DeserializeObject<Dictionary<int, byte>>(metric)
-    obj
 
 let getMetric metric =
-    let obj = JsonConvert.DeserializeObject<Dictionary<string, byte>>(metric)
+    let obj = JsonConvert.DeserializeObject<Dictionary<string, int>>(metric)
     obj
 
-let appendDictionary (main: Dictionary<int, (byte*byte)>) (adding: Dictionary<int, byte>) =
+let appendDictionary (main: Dictionary<string, (int*int)>) (adding: Dictionary<string, int>) prefix =
     for elem in adding do
-        if (main.ContainsKey elem.Key)
+        let key = prefix + elem.Key
+        if (main.ContainsKey key)
         then
-            let (sum, num) = main.[elem.Key]
-            main.[elem.Key] <- (sum + elem.Value, num + 1uy)
+            let (sum, num) = main.[key]
+            main.[key] <- (sum + elem.Value, num + 1)
         else
-            main.[elem.Key] <- (elem.Value, 1uy)
+            main.[key] <- (elem.Value, 1)
 
-let loadData () =
-    let dict = Dictionary<string, byte>()
-    let metrics = Dictionary<string, UserData>()
-    let linesWithAnswers  = File.ReadLines("../../../data/mlboot_train_answers.tsv")
-    for line in linesWithAnswers do
-        let values = line.Split('\t')
-        if
-            values.[0] <> "cuid"
-        then
-            dict.[values.[0]] <- values.[1] |> byte
-
-    let linesWithData  = File.ReadLines("../../../data/mlboot_data.tsv")
-    for line in linesWithData do
-        let values = line.Split('\t')
-        let id = values.[0]
-        if
-            dict.ContainsKey(id)
-        then
-            let metrics2 = deserializeMetric values.[2]
-            let metrics3 = deserializeMetric values.[3]
-            let metrics4 = deserializeMetric values.[4]
-            if metrics.ContainsKey(id)
-            then
-                let metricsDict = metrics.[id].Metrics
-                metrics2 |> appendDictionary metricsDict
-                metrics3 |> appendDictionary metricsDict
-                metrics4 |> appendDictionary metricsDict
-            else
-
-                let metricsDist = Dictionary<int, (byte*byte)>()
-                metrics2 |> appendDictionary metricsDist
-                metrics3 |> appendDictionary metricsDist
-                metrics4 |> appendDictionary metricsDist
-                metrics.[id] <-
-                                {
-                                    Metrics = metricsDist
-                                    Vote = dict.[id]
-                                }
-    metrics
-
-let extractMetrics (metric: Dictionary<string, byte>) =
+let extractMetrics (metric: Dictionary<string, int>) =
     metric.Keys
 
 let addMetricToSet (dict: Dictionary<string, int>) (goodSet: HashSet<string>) prefix metric =
@@ -86,7 +34,7 @@ let addMetricToSet (dict: Dictionary<string, int>) (goodSet: HashSet<string>) pr
         then
             count <- count + 1
             dict.[metricName] <- count
-            if count = 1000
+            if count = 10000
             then goodSet.Add(metricName) |> ignore
         else
             dict.[metricName] <- 1)
@@ -96,6 +44,7 @@ let loadDataNew() =
     let dict = Dictionary<string, byte>()
     let metricDictionary = Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase)
     let goodMetricSet = HashSet<string>(StringComparer.InvariantCultureIgnoreCase)
+    goodMetricSet.Add("label") |> ignore
 
     let addMetric = addMetricToSet metricDictionary goodMetricSet
     let mutable counter = 0
@@ -109,7 +58,6 @@ let loadDataNew() =
             dict.[values.[0]] <- values.[1] |> byte
 
     let linesWithData  = File.ReadLines("../../../data/mlboot_data.tsv")
-    //let results = seq {
     for line in linesWithData do
         counter <- counter + 1
         let res = line.Split('\t')
@@ -119,30 +67,97 @@ let loadDataNew() =
             addMetric "x" metric1
             addMetric "y" metric2
             addMetric "z" metric3
-    //}
     [0..5] |> List.iter (fun i -> goodMetricSet.Add("cat"+i.ToString()) |> ignore)
     goodMetricSet.Add("days") |> ignore
-    goodMetricSet.Add("id") |> ignore
-    goodMetricSet.Add("result") |> ignore
     goodMetricSet
+
+let fillDataSet headersPath =
+    let mutable counter = 0
+    let dict = Dictionary<string, int>()
+    let linesWithAnswers  = File.ReadLines("../../../data/mlboot_train_answers.tsv")
+    for line in linesWithAnswers do
+        let values = line.Split('\t')
+        if
+            values.[0] <> "cuid"
+        then
+            dict.[values.[0]] <- values.[1] |> int
+
+    let headersString = File.ReadAllText(headersPath)
+    let headers = headersString.Split(',')
+
+    let linesWithData  = File.ReadLines("../../../data/mlboot_data.tsv")
+    File.WriteAllText("../../../result/dataset.csv", String.Empty);
+    use fs = File.OpenWrite("../../../result/dataset.csv")
+    use sr = new StreamWriter(fs)
+    sr.WriteLine(headersString)
+
+    let userValues = Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase)
+    let userValuesCounter = Dictionary<string, (int*int)>()
+    headers |> Seq.iter (fun elem -> userValues.Add(elem, 0))
+
+    let mutable currentId = ""
+    let mutable touched = false
+
+    for line in linesWithData do
+        let res = line.Split('\t')
+        let [| id; category; metric1; metric2; metric3; days |] = res
+        if dict.ContainsKey(id)
+        then
+            if currentId <> id && currentId <> ""
+            then
+                for kv in userValuesCounter do
+                    let (sum, count)= kv.Value
+                    if userValues.ContainsKey(kv.Key)
+                    then
+                        userValues.[kv.Key] <- sum / count
+                        touched <- true
+
+                if touched
+                then
+                    let str = String.Join(',', userValues.Values)
+                    sr.WriteLine(str)
+                    counter <- counter + 1
+                    if (counter % 100) = 0
+                    then sr.Flush()
+                    touched <- false
+                    userValues.Clear()
+                    userValuesCounter.Clear()
+                    headers |> Seq.iter (fun elem -> userValues.Add(elem, 0))
+
+            currentId <- id
+            let storeValues metric prefix =
+                appendDictionary userValuesCounter (metric |> getMetric) prefix
+
+            storeValues metric1 "x"
+            storeValues metric2 "y"
+            storeValues metric3 "z"
+            if userValuesCounter.ContainsKey("days")
+            then
+                let (sum, count) = userValuesCounter.["days"]
+                userValuesCounter.["days"] <- (sum + (days |> int), count + 1)
+            else
+                userValuesCounter.["days"] <- (days |> int, 1)
+
+            userValues.["cat" + category.ToString()] <- 1
+            userValues.["label"] <- (int) dict.[id]
+
+
+    sr.Flush()
+    fs.Close()
 
 [<EntryPoint>]
 let main argv =
 
-    // let headers = loadDataNew()
+    //let headers = loadDataNew()
+    //File.WriteAllText("../../../result/headers10.csv", String.Join(",",headers))
 
-    // File.WriteAllText("../../../result/headers.csv", String.Join(",",headers))
+    //let array = [1..values.Length]
+    //File.WriteAllLines("../../../result/headerswork.csv", [| headers; String.Join(',',array) |])
 
-    //let customLoader = TextLoader("abcd")
-    //customLoader.
-
-    let headers = File.ReadAllText("../../../result/headers.csv")
-    let values = headers.Split(',')
-    let array = [1..values.Length]
-    File.WriteAllLines("../../../result/headerswork.csv", [| headers; String.Join(',',array) |])
+    fillDataSet "../../../result/headers10.csv"
 
     let pipeline = LearningPipeline();
-    pipeline.Add(TextLoader("../../../result/headerswork.csv").CreateFrom(true, ',',false, true, false))
+    pipeline.Add(TextLoader("../../../result/headerswork2.csv").CreateFrom(true, ',',false, true, false))
 
     printfn "Hello World from F#!"
     0 // return an integer exit code
