c384d75a10bd99320f44c71d69565a32c9e72d5b
Vladimir Shchur <shchurvladimir@gmail.com>
2018-07-03 20:46:34 +0300

    First working version

--- Program.fs
+++ Program.fs
@@ -6,6 +6,9 @@ open System.Collections.Generic
 open Newtonsoft.Json
 open Microsoft.ML
 open Microsoft.ML.Data
+open Microsoft.ML.Trainers
+open Microsoft.ML.Runtime.Api;
+open System.Globalization
 
 let getMetric metric =
     let obj = JsonConvert.DeserializeObject<Dictionary<string, int>>(metric)
@@ -71,8 +74,7 @@ let loadDataNew() =
     goodMetricSet.Add("days") |> ignore
     goodMetricSet
 
-let fillDataSet headersPath =
-    let mutable counter = 0
+let getTrainUsers() =
     let dict = Dictionary<string, int>()
     let linesWithAnswers  = File.ReadLines("../../../data/mlboot_train_answers.tsv")
     for line in linesWithAnswers do
@@ -81,15 +83,22 @@ let fillDataSet headersPath =
             values.[0] <> "cuid"
         then
             dict.[values.[0]] <- values.[1] |> int
+    dict
 
+
+let fillDataSet (dict: Dictionary<string, int>) headersPath dataPath srPath f writeHeaders =
+    let mutable counter = 0
     let headersString = File.ReadAllText(headersPath)
     let headers = headersString.Split(',')
 
-    let linesWithData  = File.ReadLines("../../../data/mlboot_data.tsv")
-    File.WriteAllText("../../../result/dataset.csv", String.Empty);
-    use fs = File.OpenWrite("../../../result/dataset.csv")
+    let linesWithData  = File.ReadLines(dataPath)
+    File.WriteAllText(srPath, String.Empty);
+    use fs = File.OpenWrite(srPath)
     use sr = new StreamWriter(fs)
-    sr.WriteLine(headersString)
+    sr.AutoFlush <- true
+    if writeHeaders
+    then
+        sr.WriteLine(headersString)
 
     let userValues = Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase)
     let userValuesCounter = Dictionary<string, (int*int)>()
@@ -114,11 +123,10 @@ let fillDataSet headersPath =
 
                 if touched
                 then
-                    let str = String.Join(',', userValues.Values)
-                    sr.WriteLine(str)
+                    f id userValues sr
                     counter <- counter + 1
-                    if (counter % 100) = 0
-                    then sr.Flush()
+                    if counter % 100 = 0
+                    then printfn "%A done" counter
                     touched <- false
                     userValues.Clear()
                     userValuesCounter.Clear()
@@ -141,9 +149,91 @@ let fillDataSet headersPath =
             userValues.["cat" + category.ToString()] <- 1
             userValues.["label"] <- (int) dict.[id]
 
+let extractTestUsers() =
+    let dict = Dictionary<string, int>()
+    let linesWithAnswers  = File.ReadLines("../../../data/mlboot_test.tsv")
+    for line in linesWithAnswers do
+        let values = line.Split('\t')
+        if
+            values.[0] <> "cuid"
+        then
+            dict.[values.[0]] <- 1
 
-    sr.Flush()
-    fs.Close()
+    let linesWithData  = File.ReadLines("../../../data/mlboot_data.tsv")
+    use fs = File.OpenWrite("../../../result/xusers_data.csv")
+    use sr = new StreamWriter(fs)
+    for line in linesWithData do
+        let res = line.Split('\t')
+        let [| id; category; metric1; metric2; metric3; days |] = res
+        if dict.ContainsKey(id)
+        then
+            sr.WriteLine(line)
+
+let getInputs() =
+    let dict = Dictionary<string, int>()
+    let linesWithAnswers  = File.ReadLines("../../../data/mlboot_test.tsv")
+    for line in linesWithAnswers do
+        if
+            line <> "cuid"
+        then
+            dict.[line] <- 0
+    dict
+
+
+type Input =
+        [<Column("0", "Label")>]
+        val mutable Label: single
+        [<Column("1", "Features")>]
+        [<VectorType(14699)>]
+        val mutable Features: single[]
+
+        new() = { Label = 0.0f; Features = [||] }
+
+
+type Output =
+
+        [<ColumnName("Score")>]
+        val mutable Score: single
+
+        new() = { Score = 0.0f; }
+
+
+let writeDataSetRow id (userValues: Dictionary<string, int>) (sr: StreamWriter) =
+    let str = String.Join(',', userValues.Values)
+    sr.WriteLine(str)
+
+let writePrediction (model: PredictionModel<Input, Output>) id (userValues: Dictionary<string, int>) (sr: StreamWriter) =
+    let label = userValues.["label"] |> single
+    userValues.Remove("label") |> ignore
+    let input = new Input()
+    input.Label <- label
+    input.Features <- userValues.Values |> Seq.map (fun x -> (single) x) |> Seq.toArray
+    let output = model.Predict(input)
+    let score = if output.Score > 0.0f then output.Score else 0.0f
+    sr.WriteLine(id + "," + score.ToString(CultureInfo.InvariantCulture))
+
+let extractResults() =
+    let list = ResizeArray<string>()
+    let dict = Dictionary<string, single>()
+    let testUsers  = File.ReadLines("../../../data/mlboot_test1.tsv")
+    for line in testUsers do
+        list.Add(line)
+
+    let answers  = File.ReadLines("../../../result/tempResults.csv")
+    for line in answers do
+        let res = line.Split(',')
+        let [| id; value |] = res
+        dict.Add(id, value |> single )
+
+    File.WriteAllText("../../../result/final_res.csv", String.Empty);
+    use fs = File.OpenWrite("../../../result/final_res.csv")
+    use sr = new StreamWriter(fs)
+    for id in list do
+        if (dict.ContainsKey(id))
+        then sr.WriteLine(dict.[id].ToString(CultureInfo.InvariantCulture))
+        else
+            printfn "%A is missing" id
+            sr.WriteLine("0")
 
 [<EntryPoint>]
 let main argv =
@@ -151,13 +241,35 @@ let main argv =
     //let headers = loadDataNew()
     //File.WriteAllText("../../../result/headers10.csv", String.Join(",",headers))
 
-    //let array = [1..values.Length]
-    //File.WriteAllLines("../../../result/headerswork.csv", [| headers; String.Join(',',array) |])
+    //fillDataSet
+    //    (getTrainUsers())
+    //    "../../../result/headers10.csv"
+    //    "../../../data/mlboot_data.tsv"
+    //    "../../../result/dataset.csv"
+    //    writeDataSetRow
+    //    true
 
-    fillDataSet "../../../result/headers10.csv"
+    // extractTestUsers()
 
-    let pipeline = LearningPipeline();
-    pipeline.Add(TextLoader("../../../result/headerswork2.csv").CreateFrom(true, ',',false, true, false))
+
+
+    //let pipeline = LearningPipeline();
+    //pipeline.Add(TextLoader("../../../result/dataset30K.csv").CreateFrom(true, ',',false, true, false))
+    //pipeline.Add(new StochasticDualCoordinateAscentRegressor())
+    //let model = pipeline.Train<Input, Output>()
+
+
+    //fillDataSet
+    //    (getInputs())
+    //    "../../../result/headers10.csv"
+    //    "../../../result/xusers_data.tsv"
+    //    "../../../result/tempResults.csv"
+    //    (writePrediction model)
+    //    false
+
+    // "000014fe918d1f97a632a796f4948be8" is missing
+
+    extractResults()
 
     printfn "Hello World from F#!"
     0 // return an integer exit code
